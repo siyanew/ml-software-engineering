7a1fbc05be541fdfb3b7ef0ba59b9b31f1aa261c
Kasper Wendel <kasperwendel@gmail.com>
2019-10-09 19:47:14 +0200

    add seq2seq model with attention

--- pytorch/model/init_weights.py
+++ pytorch/model/init_weights.py
@@ -1,4 +1,4 @@
-from torch.nn.init import normal_, uniform_
+from torch.nn.init import constant_, normal_, uniform_
 
 from base import BaseModel
 
@@ -13,3 +13,12 @@ def normal(m: BaseModel, mean, std):
     """Initialize the weights from a normal distribution """
     for name, param in m.named_parameters():
         normal_(param.data, mean=mean, std=std)
+
+
+def normal_with_bias(m: BaseModel, mean, std, bias):
+    """Initialize the weights from a normal distribution, and bias with constant value """
+    for name, param in m.named_parameters():
+        if 'weight' in name:
+            normal_(param.data, mean=mean, std=std)
+        else:
+            constant_(param.data, bias)
