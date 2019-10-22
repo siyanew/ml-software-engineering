3a27d71ed2179cc4d2d01a7caba9d8c47e889ec0
Kasper Wendel <kasperwendel@gmail.com>
2019-10-22 23:53:41 +0200

    remove data that has to many tokens

--- pytorch/limit_data.py
+++ pytorch/limit_data.py
@@ -4,18 +4,18 @@ if __name__ == '__main__':
     DIFF_TOKENS = 100
     MSG_TOKENS = 30
 
-    src_path = 'data/Top1000-Java/java-top-1000.processed.diff'
-    trg_path = 'data/Top1000-Java/java-top-1000.processed.msg'
-    src_path_short = 'data/Top1000-Java/java-top-1000.processed.100.diff'
-    trg_path_short = 'data/Top1000-Java/java-top-1000.processed.30.diff'
+    src_path = 'data/Top1000-C#/cs-top-1000.processed.diff'
+    trg_path = 'data/Top1000-C#/cs-top-1000.processed.msg'
+    src_path_short = 'data/Top1000-C#/cs-top-1000.processed.100.diff'
+    trg_path_short = 'data/Top1000-C#/cs-top-1000.processed.30.msg'
 
-    src_file = open(src_path, encoding='utf-8')
-    trg_file = open(trg_path, encoding='utf-8')
+    src_file = open(src_path, 'r', encoding='utf-8')
+    trg_file = open(trg_path, 'r', encoding='utf-8')
 
-    src_short = open(src_path, encoding='utf-8')
-    trg_short = open(src_path, encoding='utf-8')
+    src_short = open(src_path_short, 'w+', encoding='utf-8')
+    trg_short = open(trg_path_short, 'w+', encoding='utf-8')
 
-    for idx, (src_sent, trg_sent) in tqdm(enumerate(zip(src_file, src_path))):
+    for idx, (src_sent, trg_sent) in tqdm(enumerate(zip(src_file, trg_file))):
         diff_tokens = src_sent.strip().split(" ")
         msg_tokens = trg_sent.strip().split(" ")
 
